05950b0f87a10feeaec571fb877a218aba964411
Geek Out (brianpaul) <bjthinks@gmail.com>
2010-06-06 14:42:12 -0700

    Add the ability to evaluate functions using intervals.

--- function.py
+++ function.py
@@ -1,4 +1,5 @@
 import abc
+from interval import Interval
 import unittest
 
 
@@ -35,18 +36,28 @@ class SumFunction(Function):
 
 
 class _FunctionUnitTests(unittest.TestCase):
+    def intervals(self):
+        for p in xrange(-2, 3):
+            for q in xrange(-2, 3):
+                yield Interval(p, q)
+
     def test_const(self):
         for val in xrange(5):
             self.assertEqual(val, ConstFunction(val)(87))
+            self.assertEqual(val, ConstFunction(val)(Interval(2,6)))
 
     def test_identity(self):
         for val in xrange(5):
             self.assertEqual(val, IdentityFunction()(val))
+        for i in self.intervals():
+            self.assertEqual(i, IdentityFunction()(i))
 
     def test_sum(self):
         for val in xrange(5):
             self.assertEqual(val+34, SumFunction(ConstFunction(34), IdentityFunction())(val))
-
+        for i in self.intervals():
+            self.assertEqual(i+i, SumFunction(IdentityFunction(),
+                                              IdentityFunction())(i))
 
 if __name__ == '__main__':
     unittest.main()
