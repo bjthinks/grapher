a0a0ddec95592bd00bd2b011e9ba13b00f187e53
Geek Out (brianpaul) <bjthinks@gmail.com>
2010-06-08 17:11:00 -0700

    Added unit tests of derivative

--- function.py
+++ function.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import abc
 from interval import Interval
 import unittest
@@ -151,12 +152,24 @@ class _FunctionUnitTests(unittest.TestCase):
             for q in xrange(-2, 3):
                 yield Interval(p, q)
 
+    def numericalDerivativeTest(self, f):
+        h = 1e-6
+        tol = 1e-4
+        points = [ 2.74557456, 1.1436346, 1.743563456 ]
+        deriv = f.derivative()
+        for x in points:
+            v1 = deriv(x)
+            v2 = (f(x+h/2) - f(x-h/2)) / h
+            # We want v1 and v2 to be within tol of each other
+            self.assertTrue(abs(v1-v2) / abs(v1+v2) < tol)
+
     def test_const(self):
         for val in xrange(5):
             self.assertEqual(val, ConstFunction(val)(87))
             self.assertEqual(val, ConstFunction(val)(Interval(2,6)))
         self.assertEqual(str(ConstFunction(3)), '3')
         self.assertEqual(repr(ConstFunction(3)), 'ConstFunction(3)')
+        self.assertEqual(ConstFunction(5).derivative()(3.67), 0)
 
     def test_identity(self):
         for val in xrange(5):
@@ -165,6 +178,8 @@ class _FunctionUnitTests(unittest.TestCase):
             self.assertEqual(i, IdentityFunction()(i))
         self.assertEqual(str(IdentityFunction()), 'x')
         self.assertEqual(repr(IdentityFunction()), 'IdentityFunction()')
+        self.assertEqual(IdentityFunction().derivative()(6), 1)
+        self.numericalDerivativeTest(IdentityFunction())
 
     def test_sum(self):
         for val in xrange(5):
@@ -177,6 +192,12 @@ class _FunctionUnitTests(unittest.TestCase):
                          '(1 + 2)')
         self.assertEqual(repr(SumFunction(ConstFunction(1), ConstFunction(2))),
                          'SumFunction(ConstFunction(1), ConstFunction(2))')
+        self.assertEqual(SumFunction(IdentityFunction(),
+                                     ConstFunction(5)).derivative()(8), 1)
+        self.numericalDerivativeTest(SumFunction(IdentityFunction(),
+                                                 IdentityFunction()))
+        self.numericalDerivativeTest(SumFunction(IdentityFunction(),
+                                                 ConstFunction(5)))
 
     def test_product(self):
         for val in xrange(5):
@@ -192,6 +213,12 @@ class _FunctionUnitTests(unittest.TestCase):
         self.assertEqual(repr(ProductFunction(ConstFunction(1),
                                               ConstFunction(2))),
                          'ProductFunction(ConstFunction(1), ConstFunction(2))')
+        self.assertEqual(ProductFunction(IdentityFunction(),
+                                         ConstFunction(4)).derivative()(7), 4)
+        self.numericalDerivativeTest(ProductFunction(IdentityFunction(),
+                                                     IdentityFunction()))
+        self.numericalDerivativeTest(ProductFunction(IdentityFunction(),
+                                                     ConstFunction(5)))
 
     def test_quotient(self):
         for v in xrange(5):
@@ -205,6 +232,14 @@ class _FunctionUnitTests(unittest.TestCase):
                                                ConstFunction(2))),
                          'QuotientFunction(ConstFunction(1), ConstFunction(2))'
                          )
+        # Floating point is exact for small powers of two
+        self.assertEqual(QuotientFunction(ConstFunction(1),
+                                          IdentityFunction()).derivative()(2),
+                         -0.25)
+        self.numericalDerivativeTest(QuotientFunction(ConstFunction(3),
+                                                      IdentityFunction()))
+        self.numericalDerivativeTest(QuotientFunction(IdentityFunction(),
+                                                      ConstFunction(5)))
 
     def test_power(self):
         for val in xrange(5):
@@ -226,6 +261,7 @@ class _FunctionUnitTests(unittest.TestCase):
         self.assertEqual(repr(PowerFunction(ConstFunction(1),
                                             ConstFunction(2))),
                          'PowerFunction(ConstFunction(1), ConstFunction(2))')
+        # Need some tests of derivative, once it's implemented
 
 
 if __name__ == '__main__':
