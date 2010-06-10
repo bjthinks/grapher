54247b41cdfedda0299fd7542ea3683558cac22b
Paul Berry <stereotype441@gmail.com>
2010-06-10 15:44:20 -0700

    Added code to demo.py to evaluate the function and its derivatives on intervals.

--- demo.py
+++ demo.py
@@ -1,8 +1,12 @@
 from function import Function
+from interval import Interval
 
 f = Function.quotient(Function.constant(1), Function.identity())
 for n in xrange(5):
+    deriv_name = 'f' + "'"*n
     deriv = f
     for i in xrange(n):
         deriv = deriv.derivative()
-    print 'f{0} = {1}'.format("'" * n, deriv)
+    print '{0} = {1}'.format(deriv_name, deriv)
+    interval = Interval(1, 2)
+    print '{0}({1}) = {2}'.format(deriv_name, interval, deriv(interval))
