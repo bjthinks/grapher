ad59f2a9d4bcb8a1394d437196f4ea19d4b6771d
Brian Johnson (Mac Pro) <bjthinks@gmail.com>
2010-12-05 16:23:20 -0800

    Added lorem ipsum text to the values map, to make html debugging easier

--- functiongrapher/main.py
+++ functiongrapher/main.py
@@ -14,7 +14,8 @@ class MyHandler(webapp.RequestHandler):
 	    escaped_function = cgi.escape(function, True)
 	    values = { 'escaped_function' : escaped_function,
                        'escaped_url' : cgi.escape(groups[0]),
-                       'svg_graph' : graph(function) }
+                       'svg_graph' : graph(function),
+                       'lorem_ipsum' : lorem_ipsum()}
 	    self.response.out.write(
 		    template.render('main.html', values))
 	else:
@@ -43,7 +44,6 @@ def graph(function):
 </svg>'''
     return output
 
-
 def main():
     app = webapp.WSGIApplication([
 		    (r'/(.*)', MyHandler)], debug=True)
@@ -51,3 +51,10 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+def lorem_ipsum():
+    return '''<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque libero tellus, condimentum a tempus vel, placerat ut orci. Suspendisse potenti. Ut aliquam aliquet tincidunt. Mauris sit amet nulla tristique dolor convallis faucibus quis eget mauris. Vestibulum facilisis, urna quis viverra sodales, ante sem dapibus metus, ut aliquam diam tortor eu tellus. Sed at ipsum id augue porta tempor. Mauris ornare, urna sit amet luctus adipiscing, sapien massa pretium enim, et congue nibh diam aliquet elit. Sed arcu libero, pellentesque ac iaculis sed, mattis non justo. Nulla mauris ligula, bibendum id tincidunt in, vulputate ut mi. Aenean malesuada placerat turpis et sollicitudin. Nulla rhoncus magna vel turpis eleifend eget tristique erat volutpat. Nunc sapien nunc, interdum ac dictum ac, eleifend nec diam. Nunc fringilla aliquam congue. Curabitur bibendum tellus in est vulputate sed cursus massa iaculis.</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu est diam. Aenean sapien eros, pellentesque a pulvinar vitae, sodales id diam. Mauris mattis leo pellentesque elit facilisis congue. Ut dui erat, lacinia sit amet rhoncus ut, lobortis ut elit. Mauris nec augue arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae magna diam. Pellentesque pellentesque, felis id laoreet semper, nibh magna tristique ante, a ornare nibh augue quis mi. Sed faucibus auctor felis vitae auctor.</p>
+<p>Nunc libero quam, vehicula sed elementum ornare, eleifend non massa. Proin nulla urna, fermentum sit amet eleifend non, dapibus in lorem. Mauris lobortis congue diam, id accumsan quam pharetra eu. Aenean gravida, elit at luctus rutrum, velit eros ullamcorper orci, eget tristique ante eros sed ligula. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse potenti. Proin aliquam sapien sed eros lobortis aliquet. Morbi cursus nunc ac sapien eleifend ut molestie magna tincidunt. Aenean malesuada consequat egestas. Sed viverra tincidunt scelerisque. Donec pellentesque, ante non hendrerit convallis, libero nunc dignissim risus, eu iaculis justo nisi et velit. Suspendisse at ultricies quam. Phasellus vitae magna eget urna gravida congue sodales at libero. Aliquam ullamcorper egestas elit, sit amet ultricies felis vulputate in. Maecenas pretium odio a est porta vel pulvinar est volutpat. Phasellus porttitor urna quis magna tristique ac vestibulum urna consectetur. Etiam rhoncus vehicula egestas. Nulla facilisi. Mauris lacinia tincidunt lacus quis vulputate.</p>
+<p>Pellentesque in enim ante, at scelerisque purus. Duis sollicitudin interdum nibh, ac fringilla lacus pretium et. Curabitur felis velit, gravida eget suscipit id, interdum eu libero. Morbi blandit hendrerit nisi, vel venenatis mauris sollicitudin at. Nulla facilisi. Integer sit amet lectus est. Maecenas ultrices magna a mauris dignissim nec varius sapien dapibus. Fusce vitae nulla et ligula pretium mattis. Cras tristique orci nulla. Duis arcu massa, elementum in consectetur vitae, mollis eu lacus. Pellentesque sed odio erat. Praesent egestas, risus at hendrerit lacinia, ante sapien ullamcorper elit, a dignissim nibh dolor quis sem. Curabitur sit amet ipsum eget sapien faucibus iaculis elementum id lorem. Donec condimentum augue sed erat interdum vel dictum lectus accumsan.</p>
+<p>Fusce ac nisi diam. Curabitur dignissim varius nulla, eget auctor sem sagittis nec. Cras aliquet suscipit laoreet. Curabitur lacinia sem vel quam euismod sit amet volutpat diam commodo. Nulla vulputate adipiscing magna, eu sodales urna consequat non. Aliquam quis nibh lorem. Aenean nec tellus felis. Integer in sem augue, et porta nunc. Nunc elit augue, pretium eget dictum vel, lacinia sed odio. Nam nisi tellus, sodales quis vulputate sit amet, vulputate at sem. Nunc a quam eros. In mauris velit, rutrum id tristique elementum, tristique non odio.</p>'''
