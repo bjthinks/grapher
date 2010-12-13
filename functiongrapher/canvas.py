989177a3b51cf6363ccd2323040a781cb3adfb47
Brian Johnson (MacBook Pro) <bjthinks@gmail.com>
2010-12-12 18:18:24 -0800

    Added canvas.py with stubs

---
+++ functiongrapher/canvas.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class Canvas(object):
+
+    def __init__(self):
+        pass
+
+
+class canvasTest(unittest.TestCase):
+
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
