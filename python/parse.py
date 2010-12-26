72a3e4b3dbd448ed8af1fa8f25cb256c9d22f5e5
Brian Johnson (MacBook Pro) <bjthinks@gmail.com>
2010-12-26 14:31:08 -0800

    Adding tests for basic functions

--- python/parse.py
+++ python/parse.py
@@ -1,8 +1,18 @@
+from tokenize import *
 import unittest
 
 
 class Parse(object):
-    pass
+    def __init__(self, tokens):
+        pass
+
+    def go(self):
+        pass
+
+
+class _ParseUnitTests(unittest.TestCase):
+    def test_basic_functions(self):
+        Parse([Token('variable', 'x')]).go()
 
 
 if __name__ == '__main__':
