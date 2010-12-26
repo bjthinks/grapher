d99e39fce9087d121cdbb88dc64c24ba6f613f48
Brian Johnson (MacBook Pro) <bjthinks@gmail.com>
2010-12-26 14:44:43 -0800

    Now parsing 'x' and positive numbers

--- python/parse.py
+++ python/parse.py
@@ -5,11 +5,16 @@ import unittest
 
 class Parse(object):
     def __init__(self, tokens):
-        pass
+        self.tokens = list(tokens)
 
     def go(self):
-        return Function.identity()
-
+        if self.tokens[0].type == 'variable':
+            # It must be an x, who uses other letters anyway?
+            return Function.identity()
+        elif self.tokens[0].type == 'number':
+            return Function.constant(self.tokens[0].datum)
+        else:
+            raise "hell"
 
 class _ParseUnitTests(unittest.TestCase):
     def matches(self, input_str, desired_function):
@@ -18,6 +23,8 @@ class _ParseUnitTests(unittest.TestCase):
 
     def test_basic_functions(self):
         self.matches('x', Function.identity())
+        self.matches('1', Function.constant(1.0))
+        self.matches('4', Function.constant(4.0))
 
 
 if __name__ == '__main__':
