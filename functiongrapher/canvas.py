183083f7d3e86d16cf56207e856f4a7119239fdc
Brian Johnson (MacBook Pro) <bjthinks@gmail.com>
2011-01-30 15:32:32 -0800

    Added a Path object to Canvas and refactored some tests

--- functiongrapher/canvas.py
+++ functiongrapher/canvas.py
@@ -15,6 +15,22 @@ class Line(object):
         return '<svg:line x1="{0}" y1="{1}" x2="{2}" y2="{3}" stroke="black" stroke-width="1"/>'.format(x1, y1, x2, y2)
 
 
+class Path(object):
+
+    def __init__(self, trans, start):
+        self.trans = trans
+        self.points = [start]
+
+    def line_to(self, end):
+        self.points.append(end)
+
+    def __str__(self):
+        self.d = "M{0} {1}".format(*self.trans(self.points[0]))
+        for i in xrange(1,len(self.points)):
+            self.d += " L{0} {1}".format(*self.trans(self.points[i]))
+        return '<svg:path d="{0}" stroke="black" stroke-width="1" fill="none"/>'.format(self.d)
+
+
 class Canvas(object):
 
     # This class uses "fluent style" for chaining method invocations
@@ -26,11 +42,11 @@ class Canvas(object):
         self.__xmax = xmax
         self.__ymin = ymin
         self.__ymax = ymax
-        self.__elements = []
+        self.elements = []
 
     def line(self, start, end):
         # __svg_output stores drawable objects
-        self.__elements.append(Line(self.xy_to_pixels, start, end))
+        self.elements.append(Line(self.xy_to_pixels, start, end))
         return self
 
     def lines(self, linelist):
@@ -39,14 +55,16 @@ class Canvas(object):
         return self
 
     def path(self, pointlist):
-        for i in xrange(len(pointlist)-1):
-            self.line(pointlist[i], pointlist[i+1])
+        p = Path(self.xy_to_pixels, pointlist[0])
+        for i in xrange(1,len(pointlist)):
+            p.line_to(pointlist[i])
+        self.elements.append(p)
         return self
 
     def output(self):
         return ['<svg:svg xmlns:svg="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="{0}" height="{1}">'
                 .format(self.__xpixels, self.__ypixels)] + \
-                [str(e) for e in self.__elements] + \
+                [str(e) for e in self.elements] + \
             ['</svg:svg>']
 
     def xy_to_pixels(self, xy_point):
@@ -74,6 +92,15 @@ class canvasTest(unittest.TestCase):
         self.assertEqual(line.start, (-6,10))
         self.assertEqual(line.end, (-2,12))
 
+    def test_path(self):
+        canvas = Canvas(xpixels=256, ypixels=64, xmin=-6, xmax=2, ymin=10, ymax=14)
+        path = Path(canvas.xy_to_pixels, (-6,10))
+        path.line_to((-2,10))
+        path.line_to((-2,12))
+        self.assertEqual(str(path),
+                         '<svg:path d="M0.0 64.0 L128.0 64.0 L128.0 32.0" stroke="black" stroke-width="1" fill="none"/>')
+        self.assertEqual(path.points, [(-6,10), (-2,10), (-2,12)])
+
     def test_create(self):
         Canvas()
         Canvas(xpixels=345, ypixels=654)
@@ -91,7 +118,7 @@ class canvasTest(unittest.TestCase):
                          ['<svg:svg xmlns:svg="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="200" height="300">',
                           '</svg:svg>'])
 
-    def test_old_line(self):
+    def test_canvas_line(self):
         self.assertEqual(Canvas().line((0,0), (1,1)).output(),
                          ['<svg:svg xmlns:svg="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="500" height="500">',
                           '<svg:line x1="250.0" y1="250.0" x2="375.0" y2="125.0" stroke="black" stroke-width="1"/>',
@@ -103,7 +130,7 @@ class canvasTest(unittest.TestCase):
                           '<svg:line x1="0.0" y1="500.0" x2="500.0" y2="250.0" stroke="black" stroke-width="1"/>',
                           '</svg:svg>'])
 
-    def test_lines(self):
+    def test_canvas_lines(self):
         self.assertEqual(Canvas().lines([((-1,1), (2,2)),((-2,-2), (2,0))])
                          .output(),
                          ['<svg:svg xmlns:svg="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="500" height="500">',
@@ -111,14 +138,11 @@ class canvasTest(unittest.TestCase):
                           '<svg:line x1="0.0" y1="500.0" x2="500.0" y2="250.0" stroke="black" stroke-width="1"/>',
                           '</svg:svg>'])
 
-    def test_path(self):
-        self.assertEqual(Canvas().path([(-1,1), (2,2), (-2,-2), (2,0)])
-                         .output(),
-                         ['<svg:svg xmlns:svg="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="500" height="500">',
-                          '<svg:line x1="125.0" y1="125.0" x2="500.0" y2="0.0" stroke="black" stroke-width="1"/>',
-                          '<svg:line x1="500.0" y1="0.0" x2="0.0" y2="500.0" stroke="black" stroke-width="1"/>',
-                          '<svg:line x1="0.0" y1="500.0" x2="500.0" y2="250.0" stroke="black" stroke-width="1"/>',
-                          '</svg:svg>'])
+    def test_canvas_path(self):
+        elements = Canvas().path([(-1,1), (2,2), (-2,-2), (2,0)]).elements
+        self.assertEqual(len(elements), 1)
+        self.assertEqual(type(elements[0]), Path)
+        self.assertEqual(elements[0].points, [(-1,1), (2,2), (-2,-2), (2,0)])
 
 
 if __name__ == '__main__':
