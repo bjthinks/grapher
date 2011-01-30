24281114b6597e26ff97db85dabc5e4b03a7a507
Brian Johnson (MacBook Pro) <bjthinks@gmail.com>
2011-01-30 15:38:39 -0800

    Refactored the transformation function to be an arg to svg() rather than __init__() on the Line and Path classes

--- functiongrapher/canvas.py
+++ functiongrapher/canvas.py
@@ -4,30 +4,28 @@ import unittest
 
 class Line(object):
 
-    def __init__(self, trans, start, end):
-        self.trans = trans
+    def __init__(self, start, end):
         self.start = start
         self.end = end
 
-    def __str__(self):
-        x1, y1 = self.trans(self.start)
-        x2, y2 = self.trans(self.end)
+    def svg(self, trans):
+        x1, y1 = trans(self.start)
+        x2, y2 = trans(self.end)
         return '<svg:line x1="{0}" y1="{1}" x2="{2}" y2="{3}" stroke="black" stroke-width="1"/>'.format(x1, y1, x2, y2)
 
 
 class Path(object):
 
-    def __init__(self, trans, start):
-        self.trans = trans
+    def __init__(self, start):
         self.points = [start]
 
     def line_to(self, end):
         self.points.append(end)
 
-    def __str__(self):
-        self.d = "M{0} {1}".format(*self.trans(self.points[0]))
+    def svg(self, trans):
+        self.d = "M{0} {1}".format(*trans(self.points[0]))
         for i in xrange(1,len(self.points)):
-            self.d += " L{0} {1}".format(*self.trans(self.points[i]))
+            self.d += " L{0} {1}".format(*trans(self.points[i]))
         return '<svg:path d="{0}" stroke="black" stroke-width="1" fill="none"/>'.format(self.d)
 
 
@@ -46,7 +44,7 @@ class Canvas(object):
 
     def line(self, start, end):
         # __svg_output stores drawable objects
-        self.elements.append(Line(self.xy_to_pixels, start, end))
+        self.elements.append(Line(start, end))
         return self
 
     def lines(self, linelist):
@@ -55,7 +53,7 @@ class Canvas(object):
         return self
 
     def path(self, pointlist):
-        p = Path(self.xy_to_pixels, pointlist[0])
+        p = Path(pointlist[0])
         for i in xrange(1,len(pointlist)):
             p.line_to(pointlist[i])
         self.elements.append(p)
@@ -64,7 +62,7 @@ class Canvas(object):
     def output(self):
         return ['<svg:svg xmlns:svg="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="{0}" height="{1}">'
                 .format(self.__xpixels, self.__ypixels)] + \
-                [str(e) for e in self.elements] + \
+                [e.svg(self.xy_to_pixels) for e in self.elements] + \
             ['</svg:svg>']
 
     def xy_to_pixels(self, xy_point):
@@ -86,18 +84,18 @@ class canvasTest(unittest.TestCase):
 
     def test_line(self):
         canvas = Canvas(xpixels=256, ypixels=64, xmin=-6, xmax=2, ymin=10, ymax=14)
-        line = Line(canvas.xy_to_pixels, (-6,10), (-2,12))
-        self.assertEqual(str(line),
+        line = Line((-6,10), (-2,12))
+        self.assertEqual(line.svg(canvas.xy_to_pixels),
                          '<svg:line x1="0.0" y1="64.0" x2="128.0" y2="32.0" stroke="black" stroke-width="1"/>')
         self.assertEqual(line.start, (-6,10))
         self.assertEqual(line.end, (-2,12))
 
     def test_path(self):
         canvas = Canvas(xpixels=256, ypixels=64, xmin=-6, xmax=2, ymin=10, ymax=14)
-        path = Path(canvas.xy_to_pixels, (-6,10))
+        path = Path((-6,10))
         path.line_to((-2,10))
         path.line_to((-2,12))
-        self.assertEqual(str(path),
+        self.assertEqual(path.svg(canvas.xy_to_pixels),
                          '<svg:path d="M0.0 64.0 L128.0 64.0 L128.0 32.0" stroke="black" stroke-width="1" fill="none"/>')
         self.assertEqual(path.points, [(-6,10), (-2,10), (-2,12)])
 
