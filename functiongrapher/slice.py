cf206559bee5626fe778740ef27629cd064a7280
Brian Johnson (MacBook Pro 2) <bjthinks@gmail.com>
2011-04-13 15:58:17 -0700

    Reviewing and resuming work on slice.  Fixed subtle False/None bug.

--- functiongrapher/slice.py
+++ functiongrapher/slice.py
@@ -4,7 +4,7 @@ from function import Function
 from interval import Interval
 
 
-def is_bounded(function, x_interval, y_interval, depth = 0):
+def is_bounded(function, x_interval, y_interval, depth = 12):
     '''Determine if a function is bounded by an interval, on an interval.'''
 
     if function(x_interval) in y_interval:
@@ -13,14 +13,21 @@ def is_bounded(function, x_interval, y_interval, depth = 0):
         return False
     if not function(x_interval.right) in y_interval:
         return False
-    # Under no circumstances do we recurse more than 12 times
-    if depth >= 12:
+    # Under no circumstances should we recurse more than depth times
+    if depth == 0:
         return None
     middle = sum(x_interval.points) / 2
     left_half = Interval(x_interval.left, middle)
     right_half = Interval(middle, x_interval.right)
-    return is_bounded(function, left_half, y_interval, depth + 1) and \
-        is_bounded(function, right_half, y_interval, depth + 1)
+    left_test = is_bounded(function, left_half, y_interval, depth - 1)
+    if left_test == False:
+        return False
+    right_test = is_bounded(function, right_half, y_interval, depth - 1)
+    if right_test == False:
+        return False
+    if left_test == True and right_test == True:
+        return True
+    return None
 
 
 class _SlicingUnitTests(unittest.TestCase):
@@ -60,6 +67,14 @@ class _SlicingUnitTests(unittest.TestCase):
         self.assertTrue(is_bounded(f, Interval(-1,0), Interval(-65/256,1/256)))
         # This will never finish, since it asks for the exact bounds
         self.assertTrue(is_bounded(f, Interval(-1,0), Interval(-1/4,0)) is None)
+        # g = 1/2*x^3-3/2*x
+        g = Function.sum(Function.product(Function.constant(.5),
+                                          Function.power(Function.identity(),
+                                                         Function.constant(3))),
+                         Function.product(Function.constant(-1.5),
+                                          Function.identity()))
+        self.assertEqual(is_bounded(g, Interval(-1.5,1.5), Interval(-.9,1)),
+                         False)
 
 
 if __name__ == '__main__':
